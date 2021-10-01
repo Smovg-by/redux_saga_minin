34ffa660f892ed1ea1f86ce6d90fd27499834802
Smovg-by <Smovg-by@users.noreply.github.com>
2021-10-01 22:12:00 +0300

    some notes add

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -17,17 +17,22 @@ export function* loadPlanets() {
     console.log('planets', planets)
     yield put({ type: 'SET_PLANETS', payload: planets.results })
 }
+
 // WORKER SAGA
 export function * workerSaga () {
   const task = yield fork(loadPeople)
   yield spawn(loadPlanets)
   const people = yield join(task) // в этом случае  join является блокирующим вызовом
+  const store = yield select(store => store)
   console.log('join people', people)
 }
+
 // WATCHER SAGA
 export function * watchLoadDataSaga () {
-  yield takeEvery('LOAD_DATA', workerSaga)
+  yield takeEvery('LOAD_DATA', workerSaga) // значит: при каждом срабатывании "LOAD_DATA" вызови workerSaga
+  // и пока не выполнится "LOAD_DATA" выполнение кода дальше не продолжится
 }
+
 // ROOT SAGA
 export default function * rootSaga () {
   console.log('Saga is ready!')
